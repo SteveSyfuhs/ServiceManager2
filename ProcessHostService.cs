ccec5e56437465a3079d1f2f6200641ee6b18b5f
Steve Syfuhs <steve@syfuhs.net>
2018-10-22 15:15:26 -0700

    Initial commit for running
    It's alive

---
+++ ProcessHostService.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Threading.Tasks;
+
+namespace ServiceManager2
+{
+    partial class ProcessHostService : ServiceBase
+    {
+        private readonly ServiceConfiguration config;
+
+        public ProcessHostService(ServiceConfiguration config)
+        {
+            InitializeComponent();
+
+            this.config = config;
+        }
+
+        private Process process;
+
+        protected override void OnStart(string[] args)
+        {
+            if (string.IsNullOrWhiteSpace(config.Executable))
+            {
+                throw new ArgumentException("Executable is not defined");
+            }
+
+            var commandLine = config.Executable.Split(new[] { ' ' }, 2);
+
+            var exe = commandLine[0];
+            var arguments = "";
+
+            if (commandLine.Length > 1)
+            {
+                arguments = commandLine[1];
+            }
+
+            process = ServiceManager.RunProcess(exe, arguments);
+
+            if (process != null)
+            {
+                StartLogging();
+            }
+
+            var exited = process?.WaitForExit(10 * 1000) ?? true;
+
+            if (exited)
+            {
+                base.Stop();
+            }
+        }
+
+        private void StartLogging()
+        {
+            if (!string.IsNullOrWhiteSpace(config.LogPath))
+            {
+                if ("console".Equals(config.LogPath, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    StartReaderThread(new Tuple<StreamReader, TextWriter>(process.StandardOutput, Console.Out));
+                }
+                else
+                {
+                    RedirectOutputToFile(process, config.LogPath);
+                }
+            }
+        }
+
+        protected override void OnStop()
+        {
+
+        }
+
+        public void Start()
+        {
+            OnStart(new string[0]);
+
+            process?.WaitForExit();
+        }
+
+        private static void StartReaderThread(Tuple<StreamReader, TextWriter> args)
+        {
+            new Task(
+                obj => ReadPipe((Tuple<StreamReader, TextWriter>)obj),
+                args
+            ).Start();
+        }
+
+        private void RedirectOutputToFile(Process process, string logPath)
+        {
+            var fileStream = new FileStream(logPath, FileMode.OpenOrCreate, FileAccess.Write);
+            var writer = new StreamWriter(fileStream) { AutoFlush = true };
+
+            StartReaderThread(new Tuple<StreamReader, TextWriter>(process.StandardOutput, writer));
+        }
+
+        private static void ReadPipe(Tuple<StreamReader, TextWriter> tuple)
+        {
+            while (true)
+            {
+                if (tuple.Item1.EndOfStream)
+                {
+                    return;
+                }
+
+                var str = tuple.Item1.Read();
+
+                tuple.Item2.Write((char)str);
+            }
+        }
+    }
+
+    internal static class ServiceManager
+    {
+        public static void Install(ServiceConfiguration config)
+        {
+            RunProcess("sc", $"create {config.ServiceName} displayname= \"{config.DisplayName}\" binpath= \"{config.GenerateServiceBinPath()}\" start= auto").WaitForExit();
+
+            var service = ServiceController.GetServices().FirstOrDefault(s => s.ServiceName == config.ServiceName);
+
+            if (service != null && service.Status != ServiceControllerStatus.Running)
+            {
+                service.Start();
+            }
+        }
+
+        public static void Uninstall(ServiceConfiguration config)
+        {
+            var service = ServiceController.GetServices().FirstOrDefault(s => s.ServiceName == config.ServiceName);
+
+            if (service != null &&
+                service.CanStop &&
+                service.Status != ServiceControllerStatus.Stopped &&
+                service.Status != ServiceControllerStatus.StopPending)
+            {
+                service.Stop();
+            }
+
+            RunProcess("sc", $"delete {config.ServiceName}").WaitForExit();
+        }
+
+        internal static Process RunProcess(string exe, string command)
+        {
+            return Process.Start(new ProcessStartInfo
+            {
+                FileName = exe,
+                Arguments = command,
+                UseShellExecute = false,
+                RedirectStandardError = true,
+                RedirectStandardOutput = true
+            });
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Reflection;
+using System.ServiceProcess;
+
+namespace ServiceManager2
+{
+    public enum ServiceAction
+    {
+        Install,
+        Uninstall,
+        Start,
+        Run
+    }
+
+    public class ServiceConfiguration
+    {
+        public string Executable { get; set; }
+
+        public string DisplayName { get; internal set; }
+
+        public string ServiceName { get; internal set; }
+
+        public ServiceAccount Account { get; internal set; }
+
+        public string UserName { get; internal set; }
+
+        public string Password { get; internal set; }
+
+        public ServiceAction Action { get; set; } = ServiceAction.Start;
+
+        public string LogPath { get; set; }
+
+        internal string GenerateServiceBinPath()
+        {
+            var bin = Assembly.GetExecutingAssembly().Location;
+
+            var command = $"{bin} -exe \\\"{Executable}\\\"";
+
+            if (!string.IsNullOrWhiteSpace(LogPath))
+            {
+                command += $" -log \\\"{LogPath}\\\"";
+            }
+
+            return command;
+        }
+
+        internal static ServiceConfiguration Parse(string[] args)
+        {
+            if (args.Length == 0 || args.Length % 2 != 0)
+            {
+                return null;
+            }
+
+            var config = new ServiceConfiguration();
+
+            for (var i = 0; i < args.Length; i += 2)
+            {
+                var val = args[i + 1];
+
+                switch (args[i])
+                {
+                    case "-exe":
+                        config.Executable = val;
+                        break;
+                    case "-dn":
+                        config.DisplayName = val;
+                        break;
+                    case "-sn":
+                        config.ServiceName = val;
+                        break;
+                    case "-ac":
+                        config.Account = (ServiceAccount)Enum.Parse(typeof(ServiceAccount), val);
+                        break;
+                    case "-u":
+                        config.UserName = val;
+                        break;
+                    case "-p":
+                        config.Password = val;
+                        break;
+                    case "-action":
+                        config.Action = (ServiceAction)Enum.Parse(typeof(ServiceAction), val);
+                        break;
+                    case "-log":
+                        config.LogPath = val;
+                        break;
+                }
+            }
+
+            return config;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = ServiceConfiguration.Parse(args);
+
+            if (config == null)
+            {
+                ShowHelp();
+                return;
+            }
+
+            switch (config.Action)
+            {
+                case ServiceAction.Install:
+                    InstallService(config);
+                    break;
+                case ServiceAction.Uninstall:
+                    UninstallService(config);
+                    break;
+
+                case ServiceAction.Run:
+                    RunService(config);
+                    break;
+
+                case ServiceAction.Start:
+                default:
+                    StartService(config);
+                    break;
+            }
+        }
+
+        private static void RunService(ServiceConfiguration config)
+        {
+            var service = new ProcessHostService(config);
+
+            service.Start();
+        }
+
+        private static void StartService(ServiceConfiguration config)
+        {
+            ServiceBase.Run(new ProcessHostService(config));
+        }
+
+        private static void ShowHelp()
+        {
+            Console.WriteLine("Help...");
+        }
+
+        private static void InstallService(ServiceConfiguration config)
+        {
+            ServiceManager.Install(config);
+        }
+
+        private static void UninstallService(ServiceConfiguration config)
+        {
+            ServiceManager.Uninstall(config);
+        }
+    }
+}
